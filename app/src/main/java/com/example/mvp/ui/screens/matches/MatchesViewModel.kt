7bfdfb525ce6100aa7e0cc16851777b7e49ad859
kevcorbol04 <kevincortesbolufer2004@gmail.com>
2026-05-17 02:06:47 +0200

    Organizacion correcta entre domain y ui

--- app/src/main/java/com/example/mvp/ui/screens/matches/MatchesViewModel.kt
+++ app/src/main/java/com/example/mvp/ui/screens/matches/MatchesViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.viewModelScope
 import com.example.mvp.domain.repository.MatchRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.*
+import com.example.mvp.domain.model.Match
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
